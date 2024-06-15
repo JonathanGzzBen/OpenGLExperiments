709426d64454f5d6935ce6cbe781d2c71e954eb0
JonathanGzzBen <jonathangzzben@gmail.com>
2024-06-15 00:34:17 -0600

    Fix glfwWindowHint and register glfw error callback

--- StartupCompilation/opengl_experiments.cpp
+++ StartupCompilation/opengl_experiments.cpp
@@ -22,8 +22,10 @@ auto main() -> int {
     std::exit(EXIT_FAILURE);
   }
 
-  glfwWindowHint(GLFW_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_VERSION_MINOR, 5);
+  glfwSetErrorCallback(error_callback);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   GLFWwindow *window =
       glfwCreateWindow(640, 480, EXPERIMENT_NAME, nullptr, nullptr);
