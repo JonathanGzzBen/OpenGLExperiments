85209c689cf6ba41dc8f16c27023c3e7c8494235
JonathanGzzBen <jonathangzzben@gmail.com>
2024-06-30 19:48:22 -0600

    Add texture loading to models

--- ModelLoading/main.cpp
+++ ModelLoading/main.cpp
@@ -7,8 +7,6 @@
 #include <string>
 #include <vector>
 
-#include "assimp/mesh.h"
-
 #define GLFW_INCLUDE_NONE
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -20,7 +18,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "gl_strings/gl_strings.h"
+#include "gl_strings/gl_strings.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 
@@ -109,9 +107,69 @@ using Vertex = struct Vertex {
   float x;
   float y;
   float z;
+  float tex_u;
+  float tex_v;
 };
 
-auto load_model(const std::string& path) -> std::vector<Vertex> {
+using Texture = struct Texture {
+  unsigned int id;
+  std::string type;
+  std::string path;
+};
+
+auto create_texture_from_file(const std::string& filename) -> unsigned int {
+  int width;
+  int height;
+  int nComponents;
+  auto const* image_data =
+      stbi_load((filename).c_str(), &width, &height, &nComponents, 4);
+  if (image_data == nullptr) {
+    printError("Could not load image data\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Set up texture
+  unsigned int textures[1];
+  glCreateTextures(GL_TEXTURE_2D, 1, textures);
+
+  glTextureStorage2D(textures[0], 1, GL_RGBA8, width, height);
+  glTextureSubImage2D(textures[0], 0, 0, 0, width, height, GL_RGBA,
+                      GL_UNSIGNED_BYTE, image_data);
+  stbi_image_free((void*)image_data);
+
+  if (width % 4 != 0) {
+    // Set alignment to 1 if pixels cannot be packed in a multiple of 4
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+  }
+
+  glTextureParameteri(textures[0], GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTextureParameteri(textures[0], GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTextureParameteri(textures[0], GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTextureParameteri(textures[0], GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  return textures[0];
+}
+
+auto load_material_textures(aiMaterial* mat, aiTextureType type,
+                            const std::string& type_name)
+    -> std::vector<Texture> {
+  std::vector<Texture> textures;
+
+  for (unsigned int i = 0; i < mat->GetTextureCount(type); i++) {
+    aiString str;
+    mat->GetTexture(type, i, &str);
+    Texture texture;
+    texture.id = create_texture_from_file("models/" + std::string(str.C_Str()));
+    texture.type = type_name;
+    texture.path = str.C_Str();
+    textures.push_back(texture);
+  }
+
+  return textures;
+}
+
+auto load_model(const std::string& path)
+    -> std::pair<std::vector<Vertex>, std::vector<Texture>> {
   Assimp::Importer importer;
   const aiScene* scene = importer.ReadFile(
       path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenNormals);
@@ -122,6 +180,8 @@ auto load_model(const std::string& path) -> std::vector<Vertex> {
   }
 
   std::vector<Vertex> vertices;
+  std::vector<Texture> textures;
+
   for (unsigned int i = 0; i < scene->mNumMeshes; i++) {
     aiMesh* mesh = scene->mMeshes[i];
 
@@ -131,10 +191,29 @@ auto load_model(const std::string& path) -> std::vector<Vertex> {
       vertex.y = mesh->mVertices[j].y;
       vertex.z = mesh->mVertices[j].z;
 
+      if (mesh->mTextureCoords[0] != nullptr) {
+        vertex.tex_u = mesh->mTextureCoords[0][j].x;
+        vertex.tex_v = mesh->mTextureCoords[0][j].y;
+      }
+
       vertices.push_back(vertex);
     }
+
+    if (mesh->mMaterialIndex >= 0) {
+      aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
+
+      std::vector<Texture> diffuse_maps = load_material_textures(
+          material, aiTextureType_DIFFUSE, "texture_diffuse");
+      textures.insert(textures.end(), diffuse_maps.begin(), diffuse_maps.end());
+
+      std::vector<Texture> specular_maps = load_material_textures(
+          material, aiTextureType_SPECULAR, "texture_specular");
+      textures.insert(textures.end(), specular_maps.begin(),
+                      specular_maps.end());
+    }
   }
-  return vertices;
+
+  return {vertices, textures};
 }
 
 auto main() -> int {
@@ -203,22 +282,13 @@ auto main() -> int {
   unsigned int vbos[1];
   glCreateBuffers(1, vbos);
 
-  const float data[][5] = {
-      // Position(3)      TextureCoord(2)
-      {-1.0F, 1.0F, 1.0F, 0.0F, 1.0F},  {1.0F, 1.0F, 1.0F, 1.0F, 1.0F},
-      {-1.0F, -1.0F, 1.0F, 0.0F, 0.0F}, {-1.0F, -1.0F, 1.0F, 0.0F, 0.0F},
-      {1.0F, 1.0F, 1.0F, 1.0F, 1.0F},   {1.0F, -1.0F, 1.0F, 1.0F, 0.0F},
+  // Load Model
+  stbi_set_flip_vertically_on_load(1);
+  const auto [vertices, textures] = load_model("models/spider.obj");
 
-      {1.0F, 1.0F, 1.0F, 0.0F, 1.0F},   {1.0F, 1.0F, -1.0F, 1.0F, 1.0F},
-      {1.0F, -1.0F, 1.0F, 0.0F, 0.0F},
-
-      {1.0F, -1.0F, 1.0F, 0.0F, 0.0F},  {1.0F, 1.0F, -1.0F, 1.0F, 1.0F},
-      {1.0F, -1.0F, -1.0F, 1.0F, 0.0F}
-
-  };
-
-  const auto vertices = load_model("models/spider.obj");
-  glNamedBufferStorage(vbos[0], vertices.size() * sizeof(Vertex), vertices.data(), 0);
+  glNamedBufferStorage(vbos[0],
+                       static_cast<long long>(vertices.size() * sizeof(Vertex)),
+                       vertices.data(), 0);
 
   glBindVertexArray(vaos[0]);
   glBindBuffer(GL_ARRAY_BUFFER, vbos[0]);
@@ -226,23 +296,15 @@ auto main() -> int {
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), nullptr);
   glEnableVertexAttribArray(0);
 
-  // glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
-  //                       reinterpret_cast<void*>(3 * sizeof(float)));
-  // glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
 
-  // Lo que si funcionaba
-
-  // glNamedBufferStorage(vbos[0], sizeof(data), data, 0);
-
-  // glBindVertexArray(vaos[0]);
-  // glBindBuffer(GL_ARRAY_BUFFER, vbos[0]);
-
-  // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), nullptr);
-  // glEnableVertexAttribArray(0);
-
-  // glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
-  //                       reinterpret_cast<void*>(3 * sizeof(float)));
-  // glEnableVertexAttribArray(1);
+  // Bind textures
+  for (unsigned int i = 0; i < textures.size(); i++) {
+    glActiveTexture(GL_TEXTURE0 + i);
+    glBindTexture(GL_TEXTURE_2D, textures[i].id);
+  }
 
   // Projection Matrix
   const auto projection_mat =
@@ -270,39 +332,6 @@ auto main() -> int {
     glUniformMatrix4fv(model_matrix_uniform, 1, GL_FALSE,
                        glm::value_ptr(model_matrix));
   };
-
-  // Load texture data
-  stbi_set_flip_vertically_on_load(1);
-  int width;
-  int height;
-  int nComponents;
-  auto const* image_data =
-      stbi_load("Kita.jpg", &width, &height, &nComponents, 4);
-  if (image_data == nullptr) {
-    printError("Could not load image data\n");
-    exit(EXIT_FAILURE);
-  }
-
-  // Set up texture
-  unsigned int textures[1];
-  glCreateTextures(GL_TEXTURE_2D, 1, textures);
-
-  glTextureStorage2D(textures[0], 1, GL_RGBA8, width, height);
-  glTextureSubImage2D(textures[0], 0, 0, 0, width, height, GL_RGBA,
-                      GL_UNSIGNED_BYTE, image_data);
-  stbi_image_free((void*)image_data);
-
-  if (width % 4 != 0) {
-    // Set alignment to 1 if pixels cannot be packed in a multiple of 4
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-  }
-
-  glTextureParameteri(textures[0], GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTextureParameteri(textures[0], GL_TEXTURE_WRAP_T, GL_REPEAT);
-  glTextureParameteri(textures[0], GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTextureParameteri(textures[0], GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-  glBindTexture(GL_TEXTURE_2D, textures[0]);
 
   // Set up buffers
   glClearColor(0.0F, 0.0F, 0.0F, 1.0F);
@@ -332,13 +361,15 @@ auto main() -> int {
     set_model_matrix(program, y_rotation);
 
     // Draw and swap buffers
-    // glDrawArrays(GL_TRIANGLES, 0, 12);
-    glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+    glDrawArrays(GL_TRIANGLES, 0, static_cast<int>(vertices.size()));
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
-  glDeleteTextures(1, textures);
+  // Delete and free resources
+  for (const auto& texture : textures) {
+    glDeleteTextures(1, &texture.id);
+  }
   glDeleteBuffers(1, vbos);
   glDeleteVertexArrays(1, vaos);
   glfwDestroyWindow(window);
--- ModelLoading/shaders/fragment.glsl
+++ ModelLoading/shaders/fragment.glsl
@@ -2,10 +2,14 @@
 
 layout (location = 0) out vec4 fColor;
 
-uniform sampler2D tMomoi;
-
 in vec2 vTexCoord;
 
+uniform sampler2D texture_diffuse1;
+uniform sampler2D texture_specular1;
+
 void main() {
-	fColor = texture(tMomoi, vTexCoord);
+	vec4 diffuseColor = texture(texture_diffuse1, vTexCoord);
+	// vec4 specularColor = texture(texture_specular1, vTexCoord);
+	// fColor = mix(diffuseColor, specularColor, 0.2);
+	fColor = diffuseColor;
 }
