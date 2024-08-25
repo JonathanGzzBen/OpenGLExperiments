688041f881715af883472e370411a183c2024c2a
JonathanGzzBen <jonathangzzben@gmail.com>
2024-08-25 14:09:56 -0600

    Add meshes model loading working

--- MeshesModelLoading/main.cpp
+++ MeshesModelLoading/main.cpp
@@ -7,6 +7,8 @@
 #include <string>
 #include <vector>
 
+#include "assimp/material.h"
+
 #define GLFW_INCLUDE_NONE
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -56,51 +58,103 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action,
   }
 }
 
-auto ReadFile(const std::string& filename) -> std::string {
-  const std::ifstream input(filename, std::ios::binary);
-  std::stringstream strBuf;
-  strBuf << input.rdbuf();
-  if (input.fail() && !input.eof()) {
-    printError("Error reading file \"", filename, "\"\n");
-    return "";
-  }
-  return strBuf.str();
+class Shader {
+ public:
+  unsigned int id;
+  Shader(const char* vertex_path, const char* fragment_path);
+  auto use() const -> void;
+  auto set_int(const std::string& name, int value) const -> void;
+  auto set_mat4(const std::string& name, const glm::mat4& mat) const -> void;
+
+  static auto check_compile_errors(unsigned int shader, const std::string& type)
+      -> void;
 };
 
-auto GetShaderFromFile(const std::string& filename, GLenum type) noexcept
-    -> unsigned int {
-  std::string source_code;
+Shader::Shader(const char* vertex_path, const char* fragment_path) {
+  std::string vertex_code;
+  std::string fragment_code;
+
+  std::ifstream v_shader_file;
+  std::ifstream f_shader_file;
+
+  v_shader_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+  f_shader_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
   try {
-    source_code = ReadFile(filename);
-  } catch (const std::exception& ex) {
-    printError("Error reading from file \"", filename.c_str(),
-               "\": ", ex.what(), "\n");
-    return 0;
+    v_shader_file.open(vertex_path);
+    f_shader_file.open(fragment_path);
+
+    std::stringstream v_shader_stream;
+    std::stringstream f_shader_stream;
+
+    v_shader_stream << v_shader_file.rdbuf();
+    f_shader_stream << f_shader_file.rdbuf();
+
+    v_shader_file.close();
+    f_shader_file.close();
+
+    vertex_code = v_shader_stream.str();
+    fragment_code = f_shader_stream.str();
+  } catch (std::ifstream::failure& e) {
+    std::cerr << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
   }
 
-  if (source_code.empty()) {
-    return 0;
-  }
+  const char* v_shader_code = vertex_code.c_str();
+  const char* f_shader_code = fragment_code.c_str();
 
-  const auto shader = glCreateShader(type);
-  if (shader == 0) {
-    return 0;
-  }
+  unsigned int vertex = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex, 1, &v_shader_code, nullptr);
+  glCompileShader(vertex);
+  check_compile_errors(vertex, "VERTEX");
 
-  const auto* const source_pointer = source_code.data();
-  glShaderSource(shader, 1, &(source_pointer), nullptr);
+  unsigned int fragment = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment, 1, &f_shader_code, nullptr);
+  glCompileShader(fragment);
+  check_compile_errors(fragment, "FRAGMENT");
 
-  glCompileShader(shader);
-  int compilation_succesful = 0;
-  glGetShaderiv(shader, GL_COMPILE_STATUS, &compilation_succesful);
-  if (compilation_succesful == 0) {
-    GLsizei log_length = 0;
-    const unsigned int buf_len = 1024;
-    GLchar message[buf_len];
-    glGetShaderInfoLog(shader, buf_len, &log_length, message);
-    printError("Error compiling ", filename, ": ", message, "\n");
+  id = glCreateProgram();
+  glAttachShader(id, vertex);
+  glAttachShader(id, fragment);
+
+  glLinkProgram(id);
+  check_compile_errors(id, "PROGRAM");
+
+  glDeleteShader(vertex);
+  glDeleteShader(fragment);
+}
+
+auto Shader::use() const -> void { glUseProgram(id); }
+
+auto Shader::check_compile_errors(unsigned int shader, const std::string& type)
+    -> void {
+  int success;
+  char infoLog[1024];
+  if (type != "PROGRAM") {
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (success == 0) {
+      glGetShaderInfoLog(shader, 1024, nullptr, infoLog);
+      printError("ERROR::SHADER_COMPILATION_ERROR of type: ", type, "\n",
+                 infoLog);
+    }
+  } else {
+    glGetProgramiv(shader, GL_LINK_STATUS, &success);
+    if (success == 0) {
+      glGetProgramInfoLog(shader, 1024, nullptr, infoLog);
+      printError("ERROR::PROGRAM_LINKING_ERROR of type: ", type, "\n", infoLog);
+    }
   }
-  return shader;
+}
+
+auto Shader::set_int(const std::string& name, int value) const -> void {
+  glUniform1i(glGetUniformLocation(id, name.c_str()), value);
+}
+
+auto Shader::set_mat4(const std::string& name, const glm::mat4& mat) const
+    -> void {
+  // glUniformMatrix4fv(glGetUniformLocation(id, name.c_str()), 1, GL_FALSE,
+  //                    &mat[0][0]);
+  glUniformMatrix4fv(glGetUniformLocation(id, name.c_str()), 1, GL_FALSE,
+                     glm::value_ptr(mat));
 }
 
 using Vertex = struct Vertex {
@@ -123,27 +177,124 @@ class Mesh {
   std::vector<Texture> textures;
   unsigned int vao, vbo;
   auto setup() -> void;
-  auto draw() -> void;
+  auto draw(Shader& shader) -> void;
 };
 
 class Model {
-  public:
-    Model(const std::string& path);
-    auto draw(/* Shader &shader */) -> void;
+ public:
+  Model(const std::string& path);
+  auto draw(Shader& shader) -> void;
+
  private:
   std::vector<Mesh> meshes;
   std::string directory;
   std::vector<Texture> textures_loaded;
 
-  auto load_model(const std::string& path);
-  auto process_node(aiNode* node, const aiScene* scene);
-  auto process_mesh(aiMesh* mesh, const aiScene* scene);
+  auto load_model(const std::string& path) -> void;
+  auto process_node(aiNode* node, const aiScene* scene) -> void;
+  static auto process_mesh(aiMesh* mesh, const aiScene* scene) -> Mesh;
 
-  auto loadMaterialTextures(aiMaterial* mat, aiTextureType type,
-                            std::string typeName) -> std::vector<Texture>;
+  static auto load_material_textures(aiMaterial* mat, aiTextureType type,
+                                     const std::string& type_name)
+      -> std::vector<Texture>;
+
+  static auto create_texture_from_file(const std::string& filename)
+      -> unsigned int;
 };
 
-auto create_texture_from_file(const std::string& filename) -> unsigned int {
+Model::Model(const std::string& path) { load_model(path); }
+
+auto Model::load_model(const std::string& path) -> void {
+  Assimp::Importer importer;
+
+  const aiScene* scene = importer.ReadFile(
+      path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenNormals);
+
+  if ((scene == nullptr) ||
+      ((scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE) != 0U) ||
+      (scene->mRootNode == nullptr)) {
+    printError("ERROR::ASSIMP::", importer.GetErrorString());
+    return;
+  }
+  directory = path.substr(0, path.find_last_of('/'));
+  process_node(scene->mRootNode, scene);
+}
+
+auto Model::process_node(aiNode* node, const aiScene* scene) -> void {
+  for (unsigned int i = 0; i < node->mNumMeshes; i++) {
+    aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+    meshes.push_back(process_mesh(mesh, scene));
+  }
+
+  for (unsigned int i = 0; i < node->mNumChildren; i++) {
+    process_node(node->mChildren[i], scene);
+  }
+}
+
+auto Model::process_mesh(aiMesh* mesh, const aiScene* scene) -> Mesh {
+  std::vector<Vertex> vertices;
+  // std::vector<unsigned int> indices;
+  std::vector<Texture> textures;
+
+  // Process vertices
+  for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
+    Vertex vertex;
+    vertex.x = mesh->mVertices[i].x;
+    vertex.y = mesh->mVertices[i].y;
+    vertex.z = mesh->mVertices[i].z;
+
+    if (mesh->mTextureCoords[0] != nullptr) {
+      vertex.tex_u = mesh->mTextureCoords[0][i].x;
+      vertex.tex_v = mesh->mTextureCoords[0][i].y;
+    } else {
+      vertex.tex_u = 0;
+      vertex.tex_v = 0;
+    }
+    vertices.push_back(vertex);
+  }
+
+  // Process material
+  if (mesh->mMaterialIndex >= 0) {
+    aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
+
+    // Diffuse maps
+    std::vector<Texture> diffuse_maps = load_material_textures(
+        material, aiTextureType_DIFFUSE, "texture_diffuse");
+    textures.insert(textures.end(), diffuse_maps.begin(), diffuse_maps.end());
+
+    // Specular maps
+    std::vector<Texture> specular_maps = load_material_textures(
+        material, aiTextureType_SPECULAR, "texture_specular");
+    textures.insert(textures.end(), specular_maps.begin(), specular_maps.end());
+  }
+
+  Mesh processed_mesh;
+  processed_mesh.vertices = vertices;
+  processed_mesh.textures = textures;
+  processed_mesh.setup();
+  return processed_mesh;
+}
+
+auto Model::load_material_textures(aiMaterial* mat, aiTextureType type,
+                                   const std::string& type_name)
+    -> std::vector<Texture> {
+  std::vector<Texture> textures;
+
+  for (unsigned int i = 0; i < mat->GetTextureCount(type); i++) {
+    aiString str;
+    mat->GetTexture(type, i, &str);
+    Texture texture;
+    texture.id = create_texture_from_file("models/" + std::string(str.C_Str()));
+    texture.type = type_name;
+    texture.path = str.C_Str();
+    textures.push_back(texture);
+  }
+
+  return textures;
+}
+
+auto Model::create_texture_from_file(const std::string& filename)
+    -> unsigned int {
   int width;
   int height;
   int nComponents;
@@ -176,70 +327,59 @@ auto create_texture_from_file(const std::string& filename) -> unsigned int {
   return textures[0];
 }
 
-auto load_material_textures(aiMaterial* mat, aiTextureType type,
-                            const std::string& type_name)
-    -> std::vector<Texture> {
-  std::vector<Texture> textures;
+auto Mesh::setup() -> void {
+  glCreateVertexArrays(1, &vao);
+  glCreateBuffers(1, &vbo);
 
-  for (unsigned int i = 0; i < mat->GetTextureCount(type); i++) {
-    aiString str;
-    mat->GetTexture(type, i, &str);
-    Texture texture;
-    texture.id = create_texture_from_file("models/" + std::string(str.C_Str()));
-    texture.type = type_name;
-    texture.path = str.C_Str();
-    textures.push_back(texture);
-  }
+  // Load Model
+  stbi_set_flip_vertically_on_load(1);
 
-  return textures;
+  glNamedBufferStorage(vbo,
+                       static_cast<long long>(vertices.size() * sizeof(Vertex)),
+                       vertices.data(), 0);
+
+  glBindVertexArray(vao);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), nullptr);
+  glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  glBindVertexArray(0);
 }
 
-auto load_model(const std::string& path)
-    -> std::pair<std::vector<Vertex>, std::vector<Texture>> {
-  Assimp::Importer importer;
-  const aiScene* scene = importer.ReadFile(
-      path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenNormals);
+auto Mesh::draw(Shader& shader) -> void {
+  unsigned int diffuseNr = 1;
+  unsigned int specularNr = 1;
 
-  if (scene == nullptr || ((scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE) != 0U) ||
-      scene->mRootNode == nullptr) {
-    printError("ASSIMP::ERROR::", importer.GetErrorString(), "\n");
-  }
+  for (unsigned int i = 0; i < textures.size(); i++) {
+    glActiveTexture(GL_TEXTURE0 + i);
 
-  std::vector<Vertex> vertices;
-  std::vector<Texture> textures;
-
-  for (unsigned int i = 0; i < scene->mNumMeshes; i++) {
-    aiMesh* mesh = scene->mMeshes[i];
-
-    for (unsigned int j = 0; j < mesh->mNumVertices; j++) {
-      Vertex vertex;
-      vertex.x = mesh->mVertices[j].x;
-      vertex.y = mesh->mVertices[j].y;
-      vertex.z = mesh->mVertices[j].z;
-
-      if (mesh->mTextureCoords[0] != nullptr) {
-        vertex.tex_u = mesh->mTextureCoords[0][j].x;
-        vertex.tex_v = mesh->mTextureCoords[0][j].y;
-      }
-
-      vertices.push_back(vertex);
+    std::string number;
+    std::string name = textures[i].type;
+    if (name == "texture_diffuse") {
+      number = std::to_string(diffuseNr++);
+    } else if (name == "texture_specular") {
+      number = std::to_string(specularNr++);
     }
 
-    if (mesh->mMaterialIndex >= 0) {
-      aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
-
-      std::vector<Texture> diffuse_maps = load_material_textures(
-          material, aiTextureType_DIFFUSE, "texture_diffuse");
-      textures.insert(textures.end(), diffuse_maps.begin(), diffuse_maps.end());
-
-      std::vector<Texture> specular_maps = load_material_textures(
-          material, aiTextureType_SPECULAR, "texture_specular");
-      textures.insert(textures.end(), specular_maps.begin(),
-                      specular_maps.end());
-    }
+    shader.set_int(name + number, static_cast<int>(i));
+    glBindTexture(GL_TEXTURE_2D, textures[i].id);
   }
 
-  return {vertices, textures};
+  glBindVertexArray(vao);
+  glDrawArrays(GL_TRIANGLES, 0, static_cast<int>(vertices.size()));
+  glBindVertexArray(0);
+  glActiveTexture(GL_TEXTURE0);
+}
+
+auto Model::draw(Shader& shader) -> void {
+  for (auto& mesh : meshes) {
+    mesh.draw(shader);
+  }
 }
 
 auto main() -> int {
@@ -275,89 +415,20 @@ auto main() -> int {
   glEnable(GL_DEBUG_OUTPUT);
   glDebugMessageCallback(opengl_message_callback, nullptr);
 
-  unsigned int vertex_shader =
-      GetShaderFromFile("shaders/vertex.glsl", GL_VERTEX_SHADER);
-  if (vertex_shader == 0) {
-    printError("Error building VERTEX SHADER\n");
-    exit(EXIT_FAILURE);
-  }
-  unsigned int fragment_shader =
-      GetShaderFromFile("shaders/fragment.glsl", GL_FRAGMENT_SHADER);
-  if (fragment_shader == 0) {
-    printError("Error building FRAGMENT SHADER\n");
-    exit(EXIT_FAILURE);
-  }
+  Shader shader("shaders/vertex.glsl", "shaders/fragment.glsl");
+  shader.use();
 
-  const auto program = glCreateProgram();
-  glAttachShader(program, vertex_shader);
-  glAttachShader(program, fragment_shader);
-  glLinkProgram(program);
-  int link_successful = 0;
-  glGetProgramiv(program, GL_LINK_STATUS, &link_successful);
-  if (link_successful == 0) {
-    GLsizei log_length = 0;
-    const unsigned int buf_len = 1024;
-    GLchar message[buf_len];
-    glGetProgramInfoLog(program, buf_len, &log_length, message);
-    printError("Error linking program ", program, ": ", message, "\n");
-  }
-  glUseProgram(program);
-
-  unsigned int vaos[1];
-  glCreateVertexArrays(1, vaos);
-  unsigned int vbos[1];
-  glCreateBuffers(1, vbos);
-
-  // Load Model
-  stbi_set_flip_vertically_on_load(1);
-  const auto [vertices, textures] = load_model("models/spider.obj");
-
-  glNamedBufferStorage(vbos[0],
-                       static_cast<long long>(vertices.size() * sizeof(Vertex)),
-                       vertices.data(), 0);
-
-  glBindVertexArray(vaos[0]);
-  glBindBuffer(GL_ARRAY_BUFFER, vbos[0]);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), nullptr);
-  glEnableVertexAttribArray(0);
-
-  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
-                        (void*)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
-
-  // Bind textures
-  for (unsigned int i = 0; i < textures.size(); i++) {
-    glActiveTexture(GL_TEXTURE0 + i);
-    glBindTexture(GL_TEXTURE_2D, textures[i].id);
-  }
+  Model spider_model("models/spider.obj");
 
   // Projection Matrix
   const auto projection_mat =
       glm::perspective(glm::radians(45.0F), 1.3333F, 0.1F, 10.0F);
-  const auto project_mat_uniform = glGetUniformLocation(program, "mProjection");
-  glUniformMatrix4fv(project_mat_uniform, 1, GL_FALSE,
-                     glm::value_ptr(projection_mat));
+  shader.set_mat4("mProjection", projection_mat);
 
   // View Matrix
   const auto view_matrix =
       glm::translate(glm::mat4(1.0F), glm::vec3(0.0F, 0.0F, -2.5F));
-  const auto view_matrix_uniform = glGetUniformLocation(program, "mView");
-  glUniformMatrix4fv(view_matrix_uniform, 1, GL_FALSE,
-                     glm::value_ptr((view_matrix)));
-
-  // Model Matrix
-  const auto set_model_matrix = [](unsigned int program, float y_rotation) {
-    const auto scale_matrix =
-        glm::scale(glm::mat4(1.0), glm::vec3(0.005F, 0.005F, 0.005F));
-    const auto rotate_y_matrix =
-        glm::rotate(scale_matrix, y_rotation, glm::vec3(0.0F, 1.0F, 0.0F));
-    const auto model_matrix = glm::mat4(rotate_y_matrix);
-
-    const auto model_matrix_uniform = glGetUniformLocation(program, "mModel");
-    glUniformMatrix4fv(model_matrix_uniform, 1, GL_FALSE,
-                       glm::value_ptr(model_matrix));
-  };
+  shader.set_mat4("mView", view_matrix);
 
   // Set up buffers
   glClearColor(0.0F, 0.0F, 0.0F, 1.0F);
@@ -368,7 +439,7 @@ auto main() -> int {
   const auto y_rotation_speed = 0.5F;
   auto y_rotation = 0.0F;
 
-  while (glfwWindowShouldClose(window) == GLFW_FALSE) {
+  while (glfwWindowShouldClose(window) == 0) {
     // Clear buffers
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -383,21 +454,23 @@ auto main() -> int {
       y_rotation = 0;
     }
 
-    // Prepare model matrix
-    set_model_matrix(program, y_rotation);
+    // Model matrix
+    const auto scale_matrix =
+        glm::scale(glm::mat4(1.0), glm::vec3(0.005F, 0.005F, 0.005F));
+    const auto rotate_y_matrix =
+        glm::rotate(scale_matrix, y_rotation, glm::vec3(0.0F, 1.0F, 0.0F));
+    const auto model_matrix = glm::mat4(rotate_y_matrix);
+    shader.set_mat4("mModel", model_matrix);
 
-    // Draw and swap buffers
-    glDrawArrays(GL_TRIANGLES, 0, static_cast<int>(vertices.size()));
+    // Render the model
+    spider_model.draw(shader);
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
-  // Delete and free resources
-  for (const auto& texture : textures) {
-    glDeleteTextures(1, &texture.id);
-  }
-  glDeleteBuffers(1, vbos);
-  glDeleteVertexArrays(1, vaos);
+  // TODO: Add custom destructors to call glDeleteTextures(...),
+  // glDeleteBuffers(...), glDeleteVertexArrays(...) and glDeleteShader(...)
   glfwDestroyWindow(window);
   glfwTerminate();
   return 0;
