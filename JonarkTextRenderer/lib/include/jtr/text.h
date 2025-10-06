6e1fc51818867a3391593e29b1eb788a70db5803
JonathanGzzBen <jonathangzzben@gmail.com>
2025-10-05 22:41:19 -0600

    Add TextureManager to JonarkTextRenderer

---
+++ JonarkTextRenderer/lib/include/jtr/text.h
@@ -0,0 +1,66 @@
+#ifndef TEXT_H
+#define TEXT_H
+
+#include <stb_image_write.h>
+#include <stb_truetype.h>
+
+#include <print>
+
+struct TextManager {};
+
+struct Font {
+  bool valid;
+  uint8_t* bitmap;
+  std::unique_ptr<stbtt_packedchar[]> packed_chars;
+  std::unique_ptr<stbtt_aligned_quad[]> aligned_quads;
+  int charcode_begin;
+  int charcode_count;
+};
+
+auto load_font(const unsigned char* font_binary_data, const int charcode_begin,
+               const int charcode_count, const float font_size, const int width,
+               const int height, const int font_atlas_width,
+               const int font_atlas_height) {
+  const auto number_of_fonts = stbtt_GetNumberOfFonts(font_binary_data);
+  std::println("Read {} fonts", number_of_fonts);
+  if (number_of_fonts != 1) {
+    std::println(stderr, "File doesn't have 1 font.");
+    return Font{.valid = false};
+  }
+
+  stbtt_pack_context pack_context;
+  auto* bitmap =
+      new uint8_t[static_cast<size_t>(font_atlas_width * font_atlas_height)];
+  if (stbtt_PackBegin(&pack_context, bitmap, font_atlas_width,
+                      font_atlas_height, 0, 1, nullptr) != 1) {
+    std::println(stderr, "Could not initialize font packing context");
+    return Font{.valid = false};
+  }
+
+  auto packed_chars = std::make_unique<stbtt_packedchar[]>(charcode_count);
+  stbtt_PackFontRange(&pack_context, font_binary_data, 0, font_size,
+                      charcode_begin, charcode_count, packed_chars.get());
+
+  float x_unused;
+  float y_unused;
+  auto aligned_quads = std::make_unique<stbtt_aligned_quad[]>(charcode_count);
+
+  for (int i = 0; i < charcode_count; ++i) {
+    stbtt_GetPackedQuad(&packed_chars[i], width, height, i, &x_unused,
+                        &y_unused, &aligned_quads[i], 0);
+  }
+
+  stbi_write_png("font_atlas.png", font_atlas_width, font_atlas_height, 1,
+                 bitmap, font_atlas_width);
+
+  stbtt_PackEnd(&pack_context);
+
+  return Font{.valid = true,
+              .bitmap = bitmap,
+              .packed_chars = std::move(packed_chars),
+              .aligned_quads = std::move(aligned_quads),
+              .charcode_begin = charcode_begin,
+              .charcode_count = charcode_count};
+}
+
+#endif  // TEXT_H
---
+++ JonarkTextRenderer/lib/include/jtr/texture.h
@@ -0,0 +1,89 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+#include <GL/glew.h>
+
+#include <iostream>
+#include <print>
+
+using Texture = struct Texture {
+  bool valid;
+  unsigned int texture_id;
+};
+
+using TextureHandle = int;
+
+using TextureManager = struct TextureManager {
+  bool valid;
+  Texture* textures;
+  int num_textures;
+  int max_num_textures;
+};
+
+auto texture_manager_create(const int max_num_textures) {
+  if (max_num_textures <= 0) {
+    std::println(stderr, "Invalid max_num_textures");
+    return TextureManager{.valid = false};
+  }
+  return TextureManager{
+      .valid = true,
+      .textures = new Texture[max_num_textures],
+      .num_textures = 0,
+      .max_num_textures = max_num_textures,
+  };
+}
+
+auto texture_manager_destroy_all(TextureManager& manager) {
+  for (int i = 0; i < manager.num_textures; ++i) {
+    // destroy texture
+  }
+  delete[] manager.textures;
+  manager.num_textures = 0;
+  manager.valid = false;
+}
+
+auto texture_get(const TextureManager& manager, TextureHandle handle)
+    -> Texture* {
+  if (!manager.valid) {
+    std::println(stderr, "Texture Manager not valid");
+    return nullptr;
+  }
+  if (handle < 0 || manager.max_num_textures <= handle) {
+    std::println(stderr, "Invalid handle");
+    return nullptr;
+  }
+  if (!manager.textures[handle].valid) {
+    std::println(std::cerr, "Texture for handle not valid");
+    return nullptr;
+  }
+  return &manager.textures[handle];
+}
+
+auto texture_create(TextureManager& texture_manager, const uint8_t* image_data,
+                    const int width, const int height) -> TextureHandle {
+  unsigned int texture_id;
+  glCreateTextures(GL_TEXTURE_2D, 1, &texture_id);
+  if (texture_id == 0) {
+    std::println(stderr, "Could not create texture");
+    return -1;
+  }
+
+  glTextureParameteri(texture_id, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTextureParameteri(texture_id, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTextureParameteri(texture_id, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTextureParameteri(texture_id, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+  glTextureStorage2D(texture_id, 1, GL_R8, width, height);
+  glTextureSubImage2D(texture_id, 0, 0, 0, width, height, GL_RED,
+                      GL_UNSIGNED_BYTE, image_data);
+
+  glGenerateTextureMipmap(texture_id);
+
+  const auto handle = texture_manager.num_textures++;
+  texture_manager.textures[handle] = Texture{
+      .valid = true,
+      .texture_id = texture_id,
+  };
+  return handle;
+}
+
+#endif  // TEXTURE_H
